290fe82b27085ddb9c1f73f1e27abb809ec5d9a1
tfaneye <tunde_eye@yahoo.com>
2018-04-15 21:22:08 +0100

    added Java Operator Part2 and String Operator Part1

---
+++ FirstTasteOfIntelliJProject/src/stringOperators/StringOperatorDemo.java
@@ -0,0 +1,18 @@
+package stringOperators;
+
+/**
+ * Created by inet-tech on 15/04/18.
+ */
+public class StringOperatorDemo {
+
+
+
+    public static void main(String[] args) {
+        String greeting = "Hello World!";
+        int age = 10;
+        char grade = 'A';
+
+        int len = greeting.length();
+        System.out.println(len);
+    }
+}
